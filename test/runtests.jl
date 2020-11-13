c333b9deda86aff42b808cf3c4bae47d199c6811
Rob J Goedman <goedman@mac.com>
2020-11-13 12:25:33 -0700

    Rel 1.0.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,18 @@
 using TuringModels
 using Test
 
-
 @testset "TuringModels" begin
 
 include("../scripts/00/basic_example.jl")
+include("../scripts/02/m2.1t.jl")
+#include("../scripts/04/m4.2t.jl")
+include("../scripts/08/m8.1t.jl")
+#include("../scripts/08/m8.2t.jl")
+#include("../scripts/08/m8.3t.jl")
+#include("../scripts/08/m8.4t.jl")
+#include("../scripts/10/m10.4t.jl")
+#include("../scripts/11/m11.5t.jl")
+#include("../scripts/12/m12.4t.jl")
+#include("../scripts/13/m13.6t.jl")
 
 end
