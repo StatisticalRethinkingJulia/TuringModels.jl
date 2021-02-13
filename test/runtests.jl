e1415910a987dfb32d15fab78049ecc8b8206bd2
Rob J Goedman <goedman@mac.com>
2021-02-13 06:44:25 -0700

    Fix runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,16 @@
 using TuringModels
 using Test
 
+TestDir = @__DIR__
+testfiles = readdir(joinpath(TestDir, "..", "scripts"))
+
 @testset "TuringModels" begin
+    exclude_testfiles = "non-identifiable.jl"
 
-include("../scripts/00/basic_example.jl")
-include("../scripts/02/m2.1t.jl")
-include("../scripts/04/m4.2t.jl")
-#include("../scripts/08/m8.1t.jl")
-#include("../scripts/08/m8.2t.jl")
-#include("../scripts/08/m8.3t.jl")
-#include("../scripts/08/m8.4t.jl")
-#include("../scripts/10/m10.4t.jl")
-#include("../scripts/11/m11.5t.jl")
-#include("../scripts/12/m12.4t.jl")
-#include("../scripts/13/m13.6t.jl")
-
+    for tf in testfiles
+        if tf !in exclude_testfiles
+            println("\n\ntesting $tf.\n")
+            include("../scripts/$tf")
+        end
+    end
 end
